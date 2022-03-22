6be11379d1654a3e3dff845e31f9f90922b97b7f
unknown <andrey.antochevis@gmail.com>
2022-03-22 20:35:20 -0300

    atualizacoes curso

--- JavaScript-herancas-e-interfaces/Conta.js
+++ JavaScript-herancas-e-interfaces/Conta.js
@@ -1,32 +1,36 @@
+//classe abstrata
 export class Conta{
-    constructor(saldoInicial, cliente, agencia){
+    constructor(saldoInicial, cliente, agencia) {
+        if(this.constructor == Conta) {
+            throw new Error("Você não deveria instanciar um um objeto do tipo Conta");
+        }
+
         this._saldo = saldoInicial;
         this._cliente = cliente;
         this._agencia = agencia;
     }
 
-    set cliente(novoValor){
-        if(novoValor instanceof Cliente){
+    set cliente(novoValor) {
+        if(novoValor instanceof Cliente) {
         this._cliente = novoValor;
         }
     }
 
-    get cliente(){
+    get cliente() {
         return this._cliente;
     }
 
-    get saldo(){
+    get saldo() {
         return this._saldo;
     }
 
     sacar(valor) {
-        let taxa = 1;
-        return this._sacar(valor, taxa);
+        throw new Error("O método sacar da Conta é abstrato")
     }
 
     _sacar(valor, taxa) {
         const valorSacado = taxa * valor;
-        if (this._saldo>= valorSacado){
+        if (this._saldo>= valorSacado) {
             this._saldo -= valorSacado;
             return valorSacado;
         }
@@ -38,7 +42,7 @@ export class Conta{
         this._saldo += valor;
     }
 
-    transferir(valor, conta){
+    transferir(valor, conta) {
         const valorSacado = this.sacar(valor);
         conta.depositar(valorSacado);
     }
--- JavaScript-herancas-e-interfaces/index.js
+++ JavaScript-herancas-e-interfaces/index.js
@@ -1,6 +1,7 @@
 import { Cliente } from "./Cliente.js";
 import { ContaCorrente } from "./ContaCorrente.js";
 import { ContaPoupanca } from "./ContaPoupanca.js";
+import { ContaSalario } from "./ContaSalario.js";
 
 const cliente1 = new Cliente("Ricardo", 11122233309);
 /*cliente1.nome = "Ricardo";
@@ -16,13 +17,19 @@ const contaCorrenteRicardo = new ContaCorrente(cliente1, 1001);
 /*contaCorrenteRicardo.agencia = 1001;
 contaCorrenteRicardo.cliente = cliente1;*/
 
-contaCorrenteRicardo.depositar(5000);
-contaCorrenteRicardo.sacar(1000);
+//contaCorrenteRicardo.depositar(5000);
+//contaCorrenteRicardo.sacar(1000);
 
 const contaPoupanca = new ContaPoupanca(500, cliente1, 1001);
 
-console.log(contaPoupanca);
-console.log(contaCorrenteRicardo);
+//console.log(contaPoupanca);
+//console.log(contaCorrenteRicardo);
+
+const contaSalario = new ContaSalario(cliente1);
+contaSalario.depositar(1000);
+contaSalario.sacar(500);
+
+console.log(contaSalario);
 
 
 /*const contaCorrenteAndrey = new ContaCorrente();
